8593681bdcc3a53d76d13177224316d11a83d7fc
cristian <casberry41@gmail.com>
2023-04-06 20:01:49 -0500

    added characters to js

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+//write assigned code//
+function generatePassword(){
+
+ //need array of upper and lower case//
+ //need array of numbers and special characters// 
+ //need to hold the password that is being created//
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var number = ["0123456789"];
+var special = ["#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
+var selectArray = [];
+//define the length of the chosen password//
+
+
+
+
+
+
+
+
+
+
+}
+
 
 
 function generatePassword
-//prompt for passwor dlength, stored in variable//
-//validate the user input if the user chooses <8 or >128, theyre going to restart or call the function that callsfor the length again //
-//prompt, do they want uppercase//
-//prompt, do they want lowercase//
-//prompt, do they want numbers//
-//prompt, do they want nspecial characters//
-//going to want an array of those characters types//
-//keep them seperate or have a big array//
-//need  one selected charcter type included in the password//
 
-//validate  that the user has chosen at least one charcter set, if not have them start over or call the funtion that calls for the character set//
-
-
-
-// Write password to the #password input//
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -26,5 +34,8 @@ function writePassword() {
 
 }
 
+
+var generateBtn = document.querySelector("#generate");
+ 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
