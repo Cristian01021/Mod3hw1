180aaff2e834d9372f3528c4b25564f653f3de19
cristian <casberry41@gmail.com>
2023-04-08 19:09:19 -0500

    added cahracter type select and deleted starter code but adding it back in the future

--- script.js
+++ script.js
@@ -1,41 +1,31 @@
-//write assigned code//
-function generatePassword(){
 
- //need array of upper and lower case//
- //need array of numbers and special characters// 
- //need to hold the password that is being created//
-var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
-var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
-var number = ["0123456789"];
-var special = ["#$%&'()*+,-./:;<=>?@[]^_`{|}~"];
-var selectArray = [];
-//define the length of the chosen password//
+function generatePassword() {
+  // Arrays of character types
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numbers = "0123456789";
+  var special = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
+  // Password length
+  var passwordLength = getPasswordLength();
 
+  // Character type selection
+  var selectArray = [];
+  if (confirm("Include lowercase characters?")) {
+    selectArray.push(lowercase);
+  }
+  if (confirm("Include uppercase characters?")) {
+    selectArray.push(uppercase);
+  }
+  if (confirm("Include numeric characters?")) {
+    selectArray.push(numbers);
+  }
+  if (confirm("Include special characters?")) {
+    selectArray.push(special);
+  }
 
-
-
-
-
-
-
-
-}
-
-
-
-function generatePassword
-
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-
-var generateBtn = document.querySelector("#generate");
- 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  // If no character type is selected
+  if (selectArray.length === 0) {
+    alert("You must select at least one character type");
+    return "";
+  }
